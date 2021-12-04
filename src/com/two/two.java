25a4ae93443f2f140a9256696f3f7d9646101bce
Khushi Saxena <khushisaxena123123@gmail.com>
2021-12-04 18:35:57 +0530

    Q2

---
+++ src/com/two/two.java
@@ -0,0 +1,14 @@
+package com.two;
+
+import java.util.Scanner;
+
+public class two {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Name;
+        System.out.println("Enter your Name");
+        Name =input.nextLine();
+        System.out.println("Hello,"+ Name + " greetings from command line!");;
+
+    }
+}
