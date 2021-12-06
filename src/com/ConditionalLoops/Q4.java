c3f0f53327ad96a042269ae3fa8880ec769838b5
Khushi Saxena <khushisaxena123123@gmail.com>
2021-12-06 19:22:18 +0530

    Solutions

--- src/com/ConditionalLoops/Q4.java
+++ src/com/ConditionalLoops/Q4.java
@@ -1,4 +1,17 @@
 package com.ConditionalLoops;
 
+import java.util.Scanner;
+
 public class Q4 {
+    public static void main(String[] args){
+        // area of Isosceles Triangle
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of same sided: ");
+        double side = input.nextDouble();
+        System.out.println("Enter the length of base: ");
+        double base = input.nextDouble();
+        double area = (base/4)*Math.sqrt((4*side*side)-(base*base));
+        System.out.println("The area of Isosceles Triangle is: " + area);
+        input.close();
+    }
 }
--- src/com/ConditionalLoops/Q5.java
+++ src/com/ConditionalLoops/Q5.java
@@ -1,4 +1,16 @@
 package com.ConditionalLoops;
 
+import java.util.Scanner;
+
 public class Q5 {
+    public static void main(String[] args){
+        // area of parellogram
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the base and height of the parellogram : ");
+        double b = input.nextDouble();
+        double h = input.nextDouble();
+        double area = b * h;
+        System.out.println("Area of parellogram is " + area);
+        input.close();
+    }
 }
