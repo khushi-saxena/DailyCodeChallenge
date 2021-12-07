f2dd4cc0d02c700f01e63ffa1206e5a3089e5ec6
Khushi Saxena <khushisaxena123123@gmail.com>
2021-12-07 15:42:44 +0530

    Perimeter

---
+++ src/com/ConditionalLoops/Q12.java
@@ -0,0 +1,12 @@
+package com.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter side of Square ");
+        float Side =input.nextFloat();
+        System.out.println("Perimeter of Square is "+ 4*Side);
+    }
+}
---
+++ src/com/ConditionalLoops/Q13.java
@@ -0,0 +1,12 @@
+package com.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class Q13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter side of Rhombus ");
+        float Side =input.nextFloat();
+        System.out.println("Perimeter of Rhombus is "+ 4*Side);
+    }
+}
