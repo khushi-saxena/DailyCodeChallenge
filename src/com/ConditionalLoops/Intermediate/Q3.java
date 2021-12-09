1daaf6060e1a797a16462287fed3aa56cc1ca2d0
Khushi Saxena <khushisaxena123123@gmail.com>
2021-12-09 20:54:17 +0530

    Add java programs

---
+++ src/com/ConditionalLoops/Intermediate/Q3.java
@@ -0,0 +1,20 @@
+package com.ConditionalLoops.Intermediate;
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of elements :");
+        int n = in.nextInt();
+        int sum = 0;
+        System.out.println("Enter the "+n+" elements respectively :");
+        for(int i = 0; i < n; i++){
+            int num = in.nextInt();
+            sum += num;
+        }
+        double average = (double)sum / n;
+        System.out.println(average);
+        in.close();
+    }
+}
