d6faacb76ca646580982e86365b67a4789a92eef
Khushi Saxena <khushisaxena123123@gmail.com>
2021-12-09 21:01:40 +0530

    Add java program

---
+++ src/com/ConditionalLoops/Intermediate/Q5.java
@@ -0,0 +1,21 @@
+package com.ConditionalLoops.Intermediate;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x1, x2, y1, y2;
+        double distance = 0;
+        System.out.println("Enter the coordinates of the first point (x1,y1): ");
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
+        System.out.println("Enter the coordinates of the second point (x2,y2): ");
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
+        // using the distance formula
+        distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("The distance between the two points is " + distance);
+        scanner.close();
+    }
+}
